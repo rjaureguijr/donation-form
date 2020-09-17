72e7bc476040f1a59d48c05b9e621c0a998460b8
Rudy Jauregui <rjaureguijr@gmail.com>
2020-09-17 03:54:29 -0500

    Updates

---
+++ precache-manifest.79aff6a490c7c38ab4d1468e34016633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddda1f0dc580d836689988c49a0590f1",
+    "url": "/donation-form/index.html"
+  },
+  {
+    "revision": "1f71456c9ff50d1ab3ef",
+    "url": "/donation-form/static/css/main.dd851758.chunk.css"
+  },
+  {
+    "revision": "c15b72f07ac5877b976d",
+    "url": "/donation-form/static/js/2.6e58d57f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/donation-form/static/js/2.6e58d57f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f71456c9ff50d1ab3ef",
+    "url": "/donation-form/static/js/main.8b449b5e.chunk.js"
+  },
+  {
+    "revision": "227bcdf2148398ef1c32",
+    "url": "/donation-form/static/js/runtime-main.fc77beb5.js"
+  }
+]);
