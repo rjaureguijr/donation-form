fa8455ca8a43316dd4f656e57d643b85a10dc9b9
Rudy Jauregui <rjaureguijr@gmail.com>
2020-12-19 17:09:35 -0600

    Edits to progress bar to update value. Added script for ghpages deploy.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,7 +8,7 @@ class ProgressBar extends React.Component {
       return (
         <div className="progress-bar-container">
           <div className="tooltip-point" style={{left: `${progressNew < 100 ? progressNew + .15 : 90}%`}}></div>
-          <div className="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"
+          <div className="progress-bar" role="progressbar" aria-valuenow={progressNew} aria-valuemin="0" aria-valuemax="100"
           style={{width: `${progressNew < 100 ? (progressNew + .5) : 100}%`}}></div>
         </div>
       );
